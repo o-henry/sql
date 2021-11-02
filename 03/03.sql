14c52b8635799b5cd1672ea52cd4ced31d096807
henry <cksgod16@gmail.com>
2021-11-02 15:51:00 +0900

    Update

---
+++ 03/03.sql
@@ -0,0 +1,15 @@
+CREATE TABLE NewBook (
+    bookid INTEGER,
+    bookname VARCHAR(20),
+    publisher VARCHAR(20),
+    price INTEGER
+);
+
+ALTER TABLE NewBook ADD isbn VARCHAR(13);
+ALTER TABLE NewBook MODIFY isbn INTEGER;
+ALTER TABLE NewBook DROP COLUMN isbn;
+ALTER TABLE NewBook MODIFY bookid INTEGER NOT NULL;
+ALTER TABLE NewBook ADD PRIMARY KEY(bookid);
+
+-- DROP은  테이블의 구조와 데이터를 모두 삭제한다. 데이터만 삭제하려면 DELETE를 사용해야한다.
+DROP TABLE NewBook
