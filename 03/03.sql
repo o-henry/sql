6eb9a281d61ec42bb7812adaf08dd2de68df9896
henry <cksgod16@gmail.com>
2021-11-03 20:44:18 +0900

    Update

--- 03/03.sql
+++ 03/03.sql
@@ -13,3 +13,6 @@ ALTER TABLE NewBook ADD PRIMARY KEY(bookid);
 
 -- DROP은  테이블의 구조와 데이터를 모두 삭제한다. 데이터만 삭제하려면 DELETE를 사용해야한다.
 DROP TABLE NewBook
+
+INSERT INTO Book(bookid, bookname, publisher, price) VALUES (11, '스포츠 의학', '한솔의학서적', 90000);
+INSERT INTO Book(bookid, bookname, publisher) VALUES (11, '스포츠 의학', '한솔의학서적');
