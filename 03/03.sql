a6b8f1b3792e7f3d44c5d1f81c959eb1c8c2cb09
henry <cksgod16@gmail.com>
2021-11-30 18:26:38 +0900

    Update

---
+++ 03/03.sql
@@ -0,0 +1,120 @@
+SELECT bookname, price 
+FROM Book;
+
+SELECT price, bookname
+FROM Book;
+
+SELECT bookid, bookname, publisher, price
+FROM Book;
+
+SELECT * 
+FROM Book;
+
+SELECT publisher
+FROM Book;
+
+SELECT DISTINCT publisher
+FROM Book;
+
+SELECT *
+FROM Book
+WHERE price < 20000;
+
+SELECT *
+FROM Book
+WHERE price BETWEEN 10000 AND 20000;
+
+SELECT *
+FROM Book
+WHERE price >= 10000 AND price <= 20000;
+
+SELECT *
+FROM Book
+WHERE publisher IN('굿스포츠', '대한미디어');
+
+SELECT bookname, publisher
+FROM Book
+WHERE bookname LIKE '축구의 역사';
+
+SELECT bookname, publisher
+FROM Book
+WHERE bookname LIKE '%축구%';
+
+SELECT *
+FROM Book
+WHERE bookname LIKE '%축구%' AND price >= 20000;
+
+SELECT *
+FROM Book
+ORDER BY bookname;
+
+SELECT * 
+FROM Book
+ORDER BY price, bookname;
+
+SELECT *
+FROM Book
+ORDER BY price DESC, publisher ASC;
+
+SELECT SUM(saleprice) AS 총매출
+FROM Orders;
+
+SELECT SUM(saleprice) AS 김연아_매출
+FROM Orders
+WHERE custid=2;
+
+SELECT SUM(saleprice) AS Total, 
+       AVG(saleprice) AS Average,
+       MIN(saleprice) AS Minimum,
+       MAX(saleprice) AS Maximum
+FROM Orders;
+
+SELECT COUNT(DISTINCT publisher) AS publisher
+FROM Book;
+
+-- GROUP BY를 통해 속성 값이 같은 값끼리 그룹을 만들 수 있다.
+SELECT custid, COUNT(*) AS 도서수량, SUM(saleprice) AS 총액
+FROM Orders
+GROUP BY custid;
+
+SELECT custid, COUNT(*) AS 도서수량
+FROM Orders
+WHERE saleprice >= 8000
+GROUP BY custid
+HAVING COUNT(*) >= 2;
+
+-- JOIN 은 한 테이블의 행을 다른 테이블의 행에 연결하여 두 개 이상의 테이블을 결합하는 연산이다.
+-- 동등조인의 예다.
+SELECT * 
+FROM Customer, Orders
+WHERE Customer.custid=Orders.custid
+ORDER BY Customer.custid;
+
+SELECT name, saleprice
+FROM Customer, Orders
+WHERE Customer.custid=Orders.custid;
+
+SELECT name, SUM(saleprice) AS 총합
+FROM Customer, Orders
+WHERE Customer.custid=Orders.custid
+GROUP BY Customer.name
+ORDER BY Customer.name;
+
+SELECT Customer.name, Book.bookname
+FROM Customer, Orders, Book
+WHERE Orders.bookid=Book.bookid AND Customer.custid=Orders.custid
+ORDER BY Customer.name;
+
+SELECT Customer.name, Book.bookname
+FROM Customer, Book, Orders
+WHERE Customer.custid=Orders.custid AND Book.price=20000 AND Book.bookid=Orders.bookid;
+
+SELECT bookname
+FROM Book
+WHERE price=(SELECT MAX(price) FROM Book);
+
+SELECT name
+FROM Customer
+WHERE custid IN (SELECT custid FROM Orders);
+
+-- 대한미디어에서 출판한 도서를 구매하고 고객의 이름을 보이시오.
